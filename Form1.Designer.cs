38521d74bf6f660d6d7f35a976fcbf07724c43db
rddmoon <yaniar0416@gmail.com>
2021-03-26 23:01:22 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace CurrencyConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fromComboBox = new System.Windows.Forms.ComboBox();
+            this.toComboBox = new System.Windows.Forms.ComboBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelConverted = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(170, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 65);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Converter";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(238, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox
+            // 
+            this.textBox.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox.Location = new System.Drawing.Point(213, 258);
+            this.textBox.MaxLength = 15;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(252, 34);
+            this.textBox.TabIndex = 2;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(92, 261);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Amount";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(119, 326);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 28);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "From";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(145, 396);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 28);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "To";
+            // 
+            // fromComboBox
+            // 
+            this.fromComboBox.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fromComboBox.FormattingEnabled = true;
+            this.fromComboBox.Items.AddRange(new object[] {
+            "CNY - Chinese Yuan Renminbi",
+            "EUR - Euro",
+            "IDR - Indonesian Rupiah",
+            "USD - US Dollar"});
+            this.fromComboBox.Location = new System.Drawing.Point(213, 323);
+            this.fromComboBox.Name = "fromComboBox";
+            this.fromComboBox.Size = new System.Drawing.Size(252, 36);
+            this.fromComboBox.TabIndex = 5;
+            // 
+            // toComboBox
+            // 
+            this.toComboBox.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toComboBox.FormattingEnabled = true;
+            this.toComboBox.Items.AddRange(new object[] {
+            "CNY - Chinese Yuan Renminbi",
+            "EUR - Euro",
+            "IDR - Indonesian Rupiah",
+            "USD - US Dollar"});
+            this.toComboBox.Location = new System.Drawing.Point(213, 393);
+            this.toComboBox.Name = "toComboBox";
+            this.toComboBox.Size = new System.Drawing.Size(252, 36);
+            this.toComboBox.TabIndex = 5;
+            // 
+            // convertButton
+            // 
+            this.convertButton.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.convertButton.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convertButton.ForeColor = System.Drawing.SystemColors.Control;
+            this.convertButton.Location = new System.Drawing.Point(181, 464);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(234, 47);
+            this.convertButton.TabIndex = 6;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = false;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(183, 546);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(230, 32);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Converted Amount:";
+            // 
+            // labelConverted
+            // 
+            this.labelConverted.AutoSize = true;
+            this.labelConverted.BackColor = System.Drawing.SystemColors.Control;
+            this.labelConverted.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelConverted.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.labelConverted.Location = new System.Drawing.Point(249, 601);
+            this.labelConverted.Name = "labelConverted";
+            this.labelConverted.Size = new System.Drawing.Size(0, 28);
+            this.labelConverted.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 694);
+            this.Controls.Add(this.labelConverted);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.toComboBox);
+            this.Controls.Add(this.fromComboBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Currency Converter";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox fromComboBox;
+        private System.Windows.Forms.ComboBox toComboBox;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelConverted;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CurrencyConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            
+            if (string.IsNullOrEmpty(textBox.Text))
+            {
+                MessageBox.Show("Please fill in the amount.");
+            }
+
+            if (string.IsNullOrEmpty((string)toComboBox.SelectedItem) || string.IsNullOrEmpty((string)fromComboBox.SelectedItem))
+            {
+                MessageBox.Show("Please choose the currency first.");
+            }
+
+            //from IDR to ...
+
+            if (fromComboBox.SelectedItem == "IDR - Indonesian Rupiah" && toComboBox.SelectedItem == "CNY - Chinese Yuan Renminbi")
+            {
+                Double conv = Double.Parse(textBox.Text) / 2203.65;
+                labelConverted.Text = conv.ToString("C", System.Globalization.CultureInfo.GetCultureInfo("en-cn"));
+            }
+            if (fromComboBox.SelectedItem == "IDR - Indonesian Rupiah" && toComboBox.SelectedItem == "EUR - Euro")
+            {
+                Double conv = Double.Parse(textBox.Text) / 16983.9;
+                labelConverted.Text = conv.ToString("C", System.Globalization.CultureInfo.GetCultureInfo("en-ie"));
+            }
+            if (fromComboBox.SelectedItem == "IDR - Indonesian Rupiah" && toComboBox.SelectedItem == "USD - US Dollar")
+            {
+                Double conv = Double.Parse(textBox.Text) / 14415.6;
+                labelConverted.Text = conv.ToString("C", System.Globalization.CultureInfo.GetCultureInfo("en-us"));
+            }
+            if (fromComboBox.SelectedItem == "IDR - Indonesian Rupiah" && toComboBox.SelectedItem == "IDR - Indonesian Rupiah")
+            {
+                Double conv = Double.Parse(textBox.Text); ;
+                labelConverted.Text = conv.ToString("C", System.Globalization.CultureInfo.GetCultureInfo("en-id"));
+            }
+
+            //from USD to ...
+
+            if (fromComboBox.SelectedItem == "USD - US Dollar" && toComboBox.SelectedItem == "CNY - Chinese Yuan Renminbi")
+            {
+                Double conv = Double.Parse(textBox.Text) * 6.54;
+                labelConverted.Text = conv.ToString("C", System.Globalization.CultureInfo.GetCultureInfo("en-cn"));
+            }
+            if (fromComboBox.SelectedItem == "USD - US Dollar" && toComboBox.SelectedItem == "EUR - Euro")
+            {
+                Double conv = Double.Parse(textBox.Text) * 0.85;
+                labelConverted.Text = conv.ToString("C", System.Globalization.CultureInfo.GetCultureInfo("en-ie"));
+            }
+            if (fromComboBox.SelectedItem == "USD - US Dollar" && toComboBox.SelectedItem == "IDR - Indonesian Rupiah")
+            {
+                Double conv = Double.Parse(textBox.Text) * 14415.6;
+                labelConverted.Text = conv.ToString("C", System.Globalization.CultureInfo.GetCultureInfo("en-id"));
+            }
+            if (fromComboBox.SelectedItem == "USD - US Dollar" && toComboBox.SelectedItem == "USD - US Dollar")
+            {
+                Double conv = Double.Parse(textBox.Text); ;
+                labelConverted.Text = conv.ToString("C", System.Globalization.CultureInfo.GetCultureInfo("en-us"));
+            }
+
+            //from EUR to ...
+            if (fromComboBox.SelectedItem == "EUR - Euro" && toComboBox.SelectedItem == "CNY - Chinese Yuan Renminbi")
+            {
+                Double conv = Double.Parse(textBox.Text) * 7.72;
+                labelConverted.Text = conv.ToString("C", System.Globalization.CultureInfo.GetCultureInfo("en-cn"));
+            }
+            if (fromComboBox.SelectedItem == "EUR - Euro" && toComboBox.SelectedItem == "USD - US Dollar")
+            {
+                Double conv = Double.Parse(textBox.Text) / 0.85;
+                labelConverted.Text = conv.ToString("C", System.Globalization.CultureInfo.GetCultureInfo("en-us"));
+            }
+            if (fromComboBox.SelectedItem == "EUR - Euro" && toComboBox.SelectedItem == "IDR - Indonesian Rupiah")
+            {
+                Double conv = Double.Parse(textBox.Text) * 16983.9;
+                labelConverted.Text = conv.ToString("C", System.Globalization.CultureInfo.GetCultureInfo("en-id"));
+            }
+            if (fromComboBox.SelectedItem == "EUR - Euro" && toComboBox.SelectedItem == "EUR - Euro")
+            {
+                Double conv = Double.Parse(textBox.Text); ;
+                labelConverted.Text = conv.ToString("C", System.Globalization.CultureInfo.GetCultureInfo("en-ie"));
+            }
+
+            //from CNY to ...
+            if (fromComboBox.SelectedItem == "CNY - Chinese Yuan Renminbi" && toComboBox.SelectedItem == "EUR - Euro")
+            {
+                Double conv = Double.Parse(textBox.Text) / 7.72;
+                labelConverted.Text = conv.ToString("C", System.Globalization.CultureInfo.GetCultureInfo("en-ie"));
+            }
+            if (fromComboBox.SelectedItem == "CNY - Chinese Yuan Renminbi" && toComboBox.SelectedItem == "USD - US Dollar")
+            {
+                Double conv = Double.Parse(textBox.Text) / 6.54;
+                labelConverted.Text = conv.ToString("C", System.Globalization.CultureInfo.GetCultureInfo("en-us"));
+            }
+            if (fromComboBox.SelectedItem == "CNY - Chinese Yuan Renminbi" && toComboBox.SelectedItem == "IDR - Indonesian Rupiah")
+            {
+                Double conv = Double.Parse(textBox.Text) * 2203.65;
+                labelConverted.Text = conv.ToString("C", System.Globalization.CultureInfo.GetCultureInfo("en-id"));
+            }
+            if (fromComboBox.SelectedItem == "CNY - Chinese Yuan Renminbi" && toComboBox.SelectedItem == "CNY - Chinese Yuan Renminbi")
+            {
+                Double conv = Double.Parse(textBox.Text); ;
+                labelConverted.Text = conv.ToString("C", System.Globalization.CultureInfo.GetCultureInfo("en-ie"));
+            }
+
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Please enter only numbers.");
+                textBox.Text = textBox.Text.Remove(textBox.Text.Length - 1);
+            }
+        }
+    }
+}
